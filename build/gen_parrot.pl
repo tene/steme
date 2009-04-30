e80341f909d432e59848122a28698b635594e3be
Stephen Weeks <tene@allalone.org>
2009-04-29 21:09:48 -0600

    Initial import.

---
+++ build/gen_parrot.pl
@@ -0,0 +1,80 @@
+#! perl
+
+=head1 TITLE
+
+gen_parrot.pl - script to obtain and build Parrot
+
+=head2 SYNOPSIS
+
+    perl gen_parrot.pl [--parrot --configure=options]
+
+=head2 DESCRIPTION
+
+Maintains an appropriate copy of Parrot in the parrot/ subdirectory.
+The revision of Parrot to be used in the build is given by the
+build/PARROT_REVISION file.
+
+=cut
+
+use strict;
+use warnings;
+use 5.008;
+
+#  Work out slash character to use.
+my $slash = $^O eq 'MSWin32' ? '\\' : '/';
+
+##  determine what revision of Parrot we require
+open my $REQ, "build/PARROT_REVISION"
+  || die "cannot open build/PARROT_REVISION\n";
+my $required = 0+<$REQ>;
+close $REQ;
+
+{
+    no warnings;
+    if (open my $REV, '-|', "parrot${slash}parrot_config revision") {
+        my $revision = 0+<$REV>;
+        close $REV;
+        if ($revision >= $required) {
+            print "Parrot r$revision already available (r$required required)\n";
+            exit(0);
+        }
+    }
+}
+
+print "Checking out Parrot r$required via svn...\n";
+system(qw(svn checkout -r),  $required , qw(https://svn.parrot.org/parrot/trunk parrot));
+
+chdir('parrot');
+
+
+##  If we have a Makefile from a previous build, do a 'make realclean'
+if (-f 'Makefile') {
+    my %config = read_parrot_config();
+    my $make = $config{'make'};
+    if ($make) {
+        print "\nPerforming '$make realclean' ...\n";
+        system($make, "realclean");
+    }
+}
+
+print "\nConfiguring Parrot ...\n";
+my @config_command = ($^X, 'Configure.pl', @ARGV);
+print "@config_command\n";
+system @config_command;
+
+print "\nBuilding Parrot ...\n";
+my %config = read_parrot_config();
+my $make = $config{'make'} or exit(1);
+system($make);
+
+sub read_parrot_config {
+    my %config = ();
+    if (open my $CFG, "config_lib.pasm") {
+        while (<$CFG>) {
+            if (/P0\["(.*?)"], "(.*?)"/) { $config{$1} = $2 }
+        }
+        close $CFG;
+    }
+    %config;
+}
+
---
+++ steme.pir
@@ -0,0 +1,77 @@
+=head1 TITLE
+
+steme.pir - A Steme compiler.
+
+=head2 Description
+
+This is the base file for the Steme compiler.
+
+This file includes the parsing and grammar rules from
+the src/ directory, loads the relevant PGE libraries,
+and registers the compiler under the name 'Steme'.
+
+=head2 Functions
+
+=over 4
+
+=item onload()
+
+Creates the Steme compiler using a C<PCT::HLLCompiler>
+object.
+
+=cut
+
+.HLL 'steme'
+
+.namespace [ 'Steme';'Compiler' ]
+
+.loadlib 'steme_group'
+
+.sub '' :anon :load :init
+    load_bytecode 'PCT.pbc'
+    .local pmc parrotns, hllns, exports
+    parrotns = get_root_namespace ['parrot']
+    hllns = get_hll_namespace
+    exports = split ' ', 'PAST PCT PGE'
+    parrotns.'export_to'(hllns, exports)
+.end
+
+.include 'src/gen_grammar.pir'
+.include 'src/gen_actions.pir'
+
+.sub 'onload' :anon :load :init
+    $P0 = get_hll_global ['PCT'], 'HLLCompiler'
+    $P1 = $P0.'new'()
+    $P1.'language'('steme')
+    $P0 = get_hll_namespace ['Steme';'Grammar']
+    $P1.'parsegrammar'($P0)
+    $P0 = get_hll_namespace ['Steme';'Grammar';'Actions']
+    $P1.'parseactions'($P0)
+.end
+
+=item main(args :slurpy)  :main
+
+Start compilation by passing any command line C<args>
+to the Steme compiler.
+
+=cut
+
+.sub 'main' :main
+    .param pmc args
+
+    $P0 = compreg 'steme'
+    $P1 = $P0.'command_line'(args)
+.end
+
+.include 'src/gen_builtins.pir'
+
+=back
+
+=cut
+
+# Local Variables:
+#   mode: pir
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4 ft=pir:
+
